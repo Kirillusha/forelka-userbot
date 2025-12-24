ee31b6f83f9320224da7273e90b7fc05eb33172b
makserwhyy <flowtermo@gmail.com>
2025-12-25 04:18:13 +1000

    Update username.py

--- modules/username.py
+++ modules/username.py
@@ -1,7 +1,14 @@
+from pyrogram.enums import ParseMode
+
 async def username_cmd(client, message, args):
     me = await client.get_me()
-    res = f"Username: @{me.username}" if me.username else "No username set"
-    await message.edit(res)
+    user = f"@{me.username}" if me.username else "None"
+    
+    res = (
+        f"<emoji id=5771887475421090729>👤</emoji> <b>Account</b>\n"
+        f"<blockquote><b>Username:</b> <code>{user}</code></blockquote>"
+    )
+    await message.edit(res, parse_mode=ParseMode.HTML)
 
 def register(app, commands, module_name):
     commands["username"] = {"func": username_cmd, "module": module_name}
