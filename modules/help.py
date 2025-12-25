c1bf4a2b07980054d94c87448229a0768e37facb
makserwhyy <flowtermo@gmail.com>
2025-12-26 01:48:37 +1000

    Update help.py

--- modules/help.py
+++ modules/help.py
@@ -4,20 +4,17 @@ from pyrogram.enums import ParseMode
 async def help_cmd(client, message, args):
     pref = getattr(client, "prefix", ".")
     sys_mods, ext_mods = {}, {}
-
     for cmd_name, info in client.commands.items():
         mod_name = info.get("module", "unknown")
         mod_path = getattr(sys.modules.get(mod_name), "__file__", "")
         target = ext_mods if "loaded_modules" in mod_path else sys_mods
         target.setdefault(mod_name, []).append(cmd_name)
-
     def format_mods(mods_dict):
         res = ""
         for mod, cmds in sorted(mods_dict.items()):
             cmds_str = " | ".join([f"{pref}{c}" for c in sorted(cmds)])
             res += f"<emoji id=5877468380125990242>➡️</emoji> <b>{mod}</b> (<code>{cmds_str}</code>)\n"
         return res.strip()
-
     text = f"<emoji id=5897962422169243693>👻</emoji> <b>Forelka Modules</b>\n\n"
     if sys_mods:
         text += f"<b>System:</b>\n<blockquote expandable>{format_mods(sys_mods)}</blockquote>\n\n"
@@ -25,7 +22,6 @@ async def help_cmd(client, message, args):
         text += f"<b>External:</b>\n<blockquote expandable>{format_mods(ext_mods)}</blockquote>"
     else:
         text += f"<b>External:</b>\n<blockquote>No external modules</blockquote>"
-
     await message.edit(text, parse_mode=ParseMode.HTML)
 
 def register(app, commands, module_name):
