5b48ea1e046c616b51974da303be0d9bbe9787b7
Kirillusha <168322959+Kirillusha@users.noreply.github.com>
2026-01-05 18:02:31 +0800

    Update info.py
    и еще

--- modules/info.py
+++ modules/info.py
@@ -126,21 +126,34 @@ async def info_cmd(client, message, args):
     
     try:
         if quote_media and is_web_url:
-            # Режим Quote Media: отправляем с link preview
-            # Добавляем ссылку в самом начале на отдельной строке для превью СВЕРХУ
-            text_with_preview = f"<a href=\"{banner_url}\">&#8203;</a>\n{info_text}"
+            # Режим Quote Media: превью БЕЗ видимой ссылки
+            # Используем невидимую ссылку с пробелом (работает в некоторых клиентах)
+            text_with_preview = f'<a href="{banner_url}">&#8288;</a>\n{info_text}'
             
-            await client.send_message(
-                chat_id=message.chat.id,
-                text=text_with_preview,
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=False,
-                reply_to_message_id=reply_to,
-                message_thread_id=thread_id
-            )
+            # Пробуем использовать link_preview_options если доступен
+            try:
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text=text_with_preview,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=False,
+                    reply_to_message_id=reply_to,
+                    message_thread_id=thread_id,
+                    link_preview_options={"is_disabled": False, "prefer_large_media": True, "show_above_text": True}
+                )
+            except:
+                # Fallback без link_preview_options
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text=text_with_preview,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=False,
+                    reply_to_message_id=reply_to,
+                    message_thread_id=thread_id
+                )
         
         elif is_local_file or (is_web_url and not quote_media):
-            # Режим Image Attachment: отправляем фото с caption
+            # Режим фото
             await client.send_photo(
                 chat_id=message.chat.id,
                 photo=banner_url,
@@ -151,7 +164,7 @@ async def info_cmd(client, message, args):
             )
         
         else:
-            # Fallback: только текст (если нет баннера)
+            # Только текст
             await client.send_message(
                 chat_id=message.chat.id,
                 text=info_text,
@@ -279,4 +292,4 @@ async def setinfobanner_cmd(client, message, args):
 def register(app, commands, module_name):
     """Регистрация команд"""
     commands["info"] = {"func": info_cmd, "module": module_name}
-    commands["setinfobanner"] = {"func": setinfobanner_cmd, "module": module_name}
+    commands["setinfobanner"] = {"func": setinfobanner_cmd, "module": module_name} 
